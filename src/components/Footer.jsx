7468be02bc2ebc5418ec872019bad8f890b7f775
Shrey Singhal <shreynbd12@gmail.com>
2024-12-05 19:30:08 +0530

    Footer position sticky

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaFacebookF, FaYoutube, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div className="bg-orange-500 text-white py-4">
+        <div className="bg-orange-500 text-white py-4 sticky bottom-0">
             <div className="mx-auto flex items-center justify-between px-36">
                 <p className="text-sm">© 2024 ExplorIn. All rights reserved
                 </p>
