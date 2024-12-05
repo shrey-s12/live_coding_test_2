fd56eb7b1224344db2b9ef4ee58c5cfacc2d6ad1
Shrey Singhal <shreynbd12@gmail.com>
2024-12-05 18:53:19 +0530

    Search when i click on the Find Button

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -5,13 +5,16 @@ import SearchContext from '../context/SearchContext';
 
 const JobList = () => {
 
-    const { searchTerm, searchCountry } = useContext(SearchContext);
+    const { searchTerm, searchCountry, searchTriggered } = useContext(SearchContext);
 
-    const filteredJobs = jobs.filter(
-        (job) =>
-            job.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
-            job.location.toLowerCase().includes(searchCountry.toLowerCase())
-    );
+    // If search is triggered, filter jobs by both searchTerm and searchCountry
+    const filteredJobs = searchTriggered
+        ? jobs.filter(
+            (job) =>
+                job.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
+                job.location.toLowerCase().includes(searchCountry.toLowerCase())
+        )
+        : jobs;
 
     return (
         <div className="py-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-36">
--- src/context/SearchContext.js
+++ src/context/SearchContext.js
@@ -5,13 +5,16 @@ const SearchContext = createContext();
 export const SearchProvider = ({ children }) => {
     const [searchTerm, setSearchTerm] = useState('');
     const [searchCountry, setSearchCountry] = useState('');
+    const [searchTriggered, setSearchTriggered] = useState(false);
 
     return (
         <SearchContext.Provider value={{
             searchTerm,
             setSearchTerm,
             searchCountry,
-            setSearchCountry
+            setSearchCountry,
+            searchTriggered,
+            setSearchTriggered
         }}>
             {children}
         </SearchContext.Provider>
