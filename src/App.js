583730f912cc87b3da2d5b5fe0d867b73360c525
Shrey Singhal <shreynbd12@gmail.com>
2024-12-05 19:22:06 +0530

    Add Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Searchbar from './components/Searchbar';
 import JobList from './components/JobList';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Searchbar />
       <JobList />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { FaFacebookF, FaYoutube, FaInstagram, FaTwitter } from "react-icons/fa";
+
+const Footer = () => {
+    return (
+        <div className="bg-orange-500 text-white py-4">
+            <div className="mx-auto flex items-center justify-between px-36">
+                <p className="text-sm">© 2024 ExplorIn. All rights reserved
+                </p>
+                <div className="flex space-x-4">
+                    <a
+                        href="https://facebook.com"
+                        target="_blank"
+                        className="text-white hover:text-purple-500"
+                    >
+                        <FaFacebookF size={20} />
+                    </a>
+                    <a
+                        href="https://youtube.com"
+                        target="_blank"
+                        className="text-white hover:text-purple-500"
+                    >
+                        <FaYoutube size={20} />
+                    </a>
+                    <a
+                        href="https://instagram.com"
+                        target="_blank"
+                        className="text-white hover:text-purple-500"
+                    >
+                        <FaInstagram size={20} />
+                    </a>
+                    <a
+                        href="https://twitter.com"
+                        target="_blank"
+                        className="text-white hover:text-purple-500"
+                    >
+                        <FaTwitter size={20} />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
